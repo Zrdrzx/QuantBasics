b184812e8bc5776550106f7db0f5320a0d6f7b12
Zrdrzx <129935328+Zrdrzx@users.noreply.github.com>
2023-10-13 14:48:59 +0200

    MULTI THREADING ADDED

--- QuantBasics/QuantBasics.py
+++ QuantBasics/QuantBasics.py
@@ -1,7 +1,12 @@
 
+from token import STAR
 import pandas as pd
 import requests
+import pytz
 from bs4 import BeautifulSoup
+from datetime import datetime
+import yfinance
+import threading
 
 
 def get_sp500_tickers():
@@ -12,12 +17,8 @@ def get_sp500_tickers():
     tickers = list(df[0].Symbol)
     return tickers
 
-
-tickers = get_sp500_tickers()
-
-
 def get_history(ticker, period_start, period_end, granularity="1d"):
-    import yfinance
+    
     df = yfinance.Ticker(ticker).history(
         start=period_start,
         end=period_end,
@@ -42,15 +43,34 @@ def get_history(ticker, period_start, period_end, granularity="1d"):
     return df
     #index datetime open high low close volume
 
-import pytz
-from datetime import datetime
+def get_histories(tickers, period_start, period_end, granularity="1d"):
+    dfs=[None]*len(tickers)
+    def _helper(i):
+        print(tickers[i])
+        df = get_history(
+            tickers[i], 
+            period_start[i], 
+            period_end[i], 
+            granularity=granularity
+        )
+        dfs[i] = df
+    threads=[threading.Thread(target=_helper, args=(i,)) for i in range(len(tickers))]
+    [Thread.start() for Thread in threads]
+    [Thread.join() for Thread in threads]
+    tickers = [tickers[i] for i in range(len(tickers)) if not dfs[i].empty]
+    dfs = [df for df in dfs if not df.empty]
+    return tickers, dfs
 
+def get_ticker_dfs(start, end):
+    tickers = get_sp500_tickers()
+    starts=[start]*len(tickers)
+    ends=[start]*len(tickers)
+    tickers, dfs = get_histories(tickers, starts,ends,granularity="1d")
+    return tickers, {ticker:df for ticker, df in zip(tickers,dfs)}
+    
 period_start = datetime(2010, 1, 1, tzinfo=pytz.utc)
 period_end = datetime(2023, 1, 1, tzinfo=pytz.utc)
-
-
-for ticker in tickers:
-    df = get_history(ticker, period_start, period_end)
-    print(ticker,df)
+tickers, ticker_dfs = get_ticker_dfs(start=period_start, end=period_end)
+print(ticker_dfs)        
 
 # print(df[0].to_json(orient='records'))#
